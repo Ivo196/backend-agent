e327b7f5c3b099bf32ef63b60f604921ffd392e8
Ivo196 <ivo.tonioni@gmail.com>
2025-04-26 11:32:30 +0200

    fix api

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from agent import process_chat, agentExecutor 
-from langchain_core.messages import HumanMessage, AIMessage
 class Post(BaseModel):
     message: str
     
-chat_history: list = []
-
 app = FastAPI(title="Max's API", description="A simple API for Max's projects")
 
 @app.get("/")
@@ -18,10 +15,7 @@ def read_root():
 def chat(post: Post):
     user_input = post.message
     print(user_input)
-    response = process_chat(agentExecutor, user_input, chat_history)
+    response = process_chat(agentExecutor, user_input)
     print(response)
-    chat_history.append(HumanMessage(content=user_input))
-    chat_history.append(AIMessage(content=response))
-    print(chat_history)
     return {"message": response}
 
